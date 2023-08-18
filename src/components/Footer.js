3e3becb26a9978cb8617d09d400f88d460811dfa
Kyle Goss <kyle.goss@reddit.com>
2023-08-18 10:08:58 -0700

    pruning template stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,24 @@
 import * as React from "react";
 import { Link } from "gatsby";
 
-import logo from "../img/logo.svg";
-import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
-import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
+import logo from "../img/logo.png";
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: "14em", height: "10em" }}
-          />
+      <footer className="footer has-background-black has-text-white-ter columns">
+        <div className="column is-6 columns">
+          <div className="column is-4"></div>
+          <div className="column">
+            <div className="content has-text-centered is-flex is-flex-direction-column is-justify-content-center">
+              <img src={logo} alt="Onyx Recipes" />
+            </div>
+          </div>
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
+        <div className="content has-text-centered has-background-black has-text-white-ter column is-6">
           <div className="container has-background-black has-text-white-ter">
             <div style={{ maxWidth: "100vw" }} className="columns">
+              <div className="column is-2"></div>
               <div className="column is-4">
                 <section className="menu">
                   <ul className="menu-list">
@@ -30,18 +28,8 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
+                      <Link className="navbar-item" to="/recipes">
+                        Recipes
                       </Link>
                     </li>
                     <li>
@@ -61,8 +49,8 @@ const Footer = class extends React.Component {
                 <section>
                   <ul className="menu-list">
                     <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
+                      <Link className="navbar-item" to="/about">
+                        About
                       </Link>
                     </li>
                     <li>
@@ -74,35 +62,13 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
+                {/* <a title="instagram" href="https://instagram.com">
                   <img
                     src={instagram}
                     alt="Instagram"
                     style={{ width: "1em", height: "1em" }}
                   />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: "1em", height: "1em" }}
-                  />
-                </a>
+                </a> */}
               </div>
             </div>
           </div>
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Layout from "../../components/Layout";
 
-// eslint-disable-next-line
-export default () => (
+const ThanksPage = () => (
   <Layout>
     <section className="section">
       <div className="container">
         <div className="content">
           <h1>Thank you!</h1>
-          <p>This is a custom thank you page for form submissions</p>
         </div>
       </div>
     </section>
   </Layout>
 );
+
+export default ThanksPage;
